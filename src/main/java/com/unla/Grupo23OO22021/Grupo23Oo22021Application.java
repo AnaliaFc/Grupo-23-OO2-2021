d5db7648e4d6c7facbbabcb0b14a851c396fde7b
Ezequiel de la Fuente <ezequieldelafuente2001@gmail.com>
2021-04-25 23:56:38 -0300

    Proyecto generado con spring initializr, con: Spring boot 2.4.5. Java 8 Spring Web Thymeleaf

---
+++ src/main/java/com/unla/Grupo23OO22021/Grupo23Oo22021Application.java
@@ -0,0 +1,13 @@
+package com.unla.Grupo23OO22021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo23Oo22021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo23Oo22021Application.class, args);
+	}
+
+}
