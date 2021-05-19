57e276654012c304cb6232a254ac8bdd902a0535
Ezequiel de la Fuente <ezequieldelafuente2001@gmail.com>
2021-05-19 12:46:49 -0300

    Agregue las entidades

---
+++ src/main/java/com/unla/Grupo23OO22021/entities/Perfil.java
@@ -0,0 +1,79 @@
+package com.unla.Grupo23OO22021.entities;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Table(name = "perfil")
+public class Perfil {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long idPerfil;
+	
+	@Column(name = "tipo")
+	private String tipo;
+	
+	@Column(name = "createat")
+	@CreationTimestamp
+	private LocalDateTime createAt;
+	
+	@Column(name = "updateat") 
+	@UpdateTimestamp
+	private LocalDateTime updateAt;
+	
+	
+	
+	public Perfil(long idPerfil, String tipo) {
+		super();
+		this.idPerfil = idPerfil;
+		this.tipo = tipo;
+	}
+
+	public Perfil(String tipo) {
+		super();
+		this.tipo = tipo;
+	}
+
+	public long getIdPerfil() {
+		return idPerfil;
+	}
+
+	public void setIdPerfil(long idPerfil) {
+		this.idPerfil = idPerfil;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public LocalDateTime getCreateAt() {
+		return createAt;
+	}
+
+	public void setCreateAt(LocalDateTime createAt) {
+		this.createAt = createAt;
+	}
+
+	public LocalDateTime getUpdateAt() {
+		return updateAt;
+	}
+
+	public void setUpdateAt(LocalDateTime updateAt) {
+		this.updateAt = updateAt;
+	}
+	
+	
+}
---
+++ src/main/java/com/unla/Grupo23OO22021/entities/Usuario.java
@@ -0,0 +1,173 @@
+package com.unla.Grupo23OO22021.entities;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+public class Usuario {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long idUsuario;
+	
+	@Column(name = "dni", unique = true)
+	private int dni;
+	
+	@Column(name = "nombre")
+	private String nombre;
+	
+	@Column(name = "apellido")
+	private String apellido;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "username", unique = true, nullable = false, length = 45)
+	private String username;
+	
+	@Column(name = "password", nullable = false, length = 100)
+	private String password;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "documento_idDocumento", nullable = false)
+	private Documento documento;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "perfil_idPerfil", nullable = false)
+	private Perfil perfil;
+	
+	@Column(name = "createat")
+	@CreationTimestamp
+	private LocalDateTime createAt;
+	
+	@Column(name = "updateat") 
+	@UpdateTimestamp
+	private LocalDateTime updateAt;
+
+	public Usuario(long idUsuario, int dni, String nombre, String apellido, String email, String username,
+			String password, Documento documento, Perfil perfil) {
+		super();
+		this.idUsuario = idUsuario;
+		this.dni = dni;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.email = email;
+		this.username = username;
+		this.password = password;
+		this.documento = documento;
+		this.perfil = perfil;
+	}
+
+	public Usuario(int dni, String nombre, String apellido, String email, String username, String password,
+			Documento documento, Perfil perfil) {
+		super();
+		this.dni = dni;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.email = email;
+		this.username = username;
+		this.password = password;
+		this.documento = documento;
+		this.perfil = perfil;
+	}
+
+	public long getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(long idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public int getDni() {
+		return dni;
+	}
+
+	public void setDni(int dni) {
+		this.dni = dni;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Documento getDocumento() {
+		return documento;
+	}
+
+	public void setDocumento(Documento documento) {
+		this.documento = documento;
+	}
+
+	public Perfil getPerfil() {
+		return perfil;
+	}
+
+	public void setPerfil(Perfil perfil) {
+		this.perfil = perfil;
+	}
+
+	public LocalDateTime getCreateAt() {
+		return createAt;
+	}
+
+	public void setCreateAt(LocalDateTime createAt) {
+		this.createAt = createAt;
+	}
+
+	public LocalDateTime getUpdateAt() {
+		return updateAt;
+	}
+
+	public void setUpdateAt(LocalDateTime updateAt) {
+		this.updateAt = updateAt;
+	}
+	
+	
+	
+	
+}
