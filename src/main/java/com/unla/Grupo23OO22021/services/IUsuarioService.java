7711fa036865b159dd44d57f6ca85e0d2383a77e
AnaliaFc <analiafc@live.com.ar>
2021-05-21 20:17:47 -0300

    Agregado el crud de usuarios

--- src/main/java/com/unla/Grupo23OO22021/services/IUsuarioService.java
+++ src/main/java/com/unla/Grupo23OO22021/services/IUsuarioService.java
@@ -1,5 +1,21 @@
 package com.unla.Grupo23OO22021.services;
 
+import java.util.List;
+
+import com.unla.Grupo23OO22021.models.UsuarioModel;
+
+
+
 public interface IUsuarioService {
+	
+public List<UsuarioModel> traerUsuarios();
+public UsuarioModel traerDocumento(int dni);
+
+public UsuarioModel traerId(long id);
+
+public UsuarioModel insertOrUpdate(UsuarioModel userModel);
+	
+public boolean remove(long id);
+	
 	
 }
