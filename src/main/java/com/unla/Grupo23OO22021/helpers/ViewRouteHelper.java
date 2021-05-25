2133883086edc62d4898632257bebc7c9a58a223
Ezequiel de la Fuente <ezequieldelafuente2001@gmail.com>
2021-05-25 11:58:57 -0300

    Hice el merge crudPerfiles -> dev

--- src/main/java/com/unla/Grupo23OO22021/helpers/ViewRouteHelper.java
+++ src/main/java/com/unla/Grupo23OO22021/helpers/ViewRouteHelper.java
@@ -2,7 +2,6 @@ package com.unla.Grupo23OO22021.helpers;
 
 public class ViewRouteHelper {
 
-<<<<<<< HEAD
 	//PERSON
 		public final static String USUARIO_INDEX = "usuario/index";
 		public final static String USUARIO_NEW = "usuario/new";
@@ -11,7 +10,6 @@ public class ViewRouteHelper {
 	//REDIRECT
 		public final static String USUARIO_ROOT = "/usuario";
 	
-=======
 	// Views
 	public final static String FORM = "viewsPerfil/form";
 	public final static String PERFILES = "viewsPerfil/listaPerfiles";
@@ -19,5 +17,4 @@ public class ViewRouteHelper {
 	// Redirects
 	public final static String ROUTE_PERFILES = "redirect:/perfil/listar";
 
->>>>>>> 27beeb6c4726a847d9b0d6bcd70ca0914cacf17b
 }
