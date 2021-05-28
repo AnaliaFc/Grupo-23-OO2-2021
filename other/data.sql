45a697ab56f09990d72ada9155d895ecb2d27402
Ezequiel de la Fuente <ezequieldelafuente2001@gmail.com>
2021-05-28 11:10:21 -0300

    Modifique el data.sql para tener un usuario con perfil auditor

--- other/data.sql
+++ other/data.sql
@@ -11,7 +11,15 @@ values('2021-05-20', "ROLE_AUDITOR",'2021-05-20');
 insert into persona(apellido, createat, dni, nombre, updateat)
 values("de la Fuente", '2020-11-11', 43200624, "Ezequiel", '2020-11-11');
 
+insert into persona(apellido, createat, dni, nombre, updateat)
+values("de la Fuente", '2020-11-11', 43200625, "Pepe", '2020-11-11');
+
 -- usuario=root password=admin
 
 insert into usuario(email, password, tipodocumento, username, id_persona, perfil_id_perfil)
 values("eze@pepe.com","$2a$10$KbD5zlop6D/W06fvqtHzLejYw3evJ3U3fWP/ffDpiiaWVQmFVhLZq", 1, "root", 1, 1 );
+
+-- usuario=user password=auditor
+
+insert into usuario(email, password, tipodocumento, username, id_persona, perfil_id_perfil)
+values("eze@pepe.com.ar","$2a$10$ePhKPkFp.Uti3gy3HI5PcupVWrckG3cEhKgbma62mjulO/K.GoalS", 2, "user", 3, 2 );
--- src/main/java/com/unla/Grupo23OO22021/test/TestBCryptPasswordEncoder.java
+++ src/main/java/com/unla/Grupo23OO22021/test/TestBCryptPasswordEncoder.java
@@ -7,7 +7,7 @@ public class TestBCryptPasswordEncoder {
 	public static void main(String[] args) {
 		
 		BCryptPasswordEncoder pe = new BCryptPasswordEncoder();
-		System.out.println(pe.encode("robotica"));
+		System.out.println(pe.encode("auditor"));
 
 	}
 
