72dde9986e8ffaeb887dcb3a37879396d603b18d
Ezequiel de la Fuente <ezequieldelafuente2001@gmail.com>
2021-05-29 00:04:54 -0300

    Agregue fragments, para no repetir html

--- src/main/java/com/unla/Grupo23OO22021/controllers/PermisoController.java
+++ src/main/java/com/unla/Grupo23OO22021/controllers/PermisoController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
+import com.unla.Grupo23OO22021.models.PermisoDiarioModel;
 import com.unla.Grupo23OO22021.models.PermisoModel;
 import com.unla.Grupo23OO22021.models.PermisoPeriodoModel;
 import com.unla.Grupo23OO22021.models.PersonaModel;
@@ -26,52 +27,77 @@ import com.unla.Grupo23OO22021.services.implementation.PermisoService;
 @Controller
 @RequestMapping("/permiso")
 public class PermisoController {
-	
+
 	@Autowired
 	@Qualifier("permisoService")
 	private PermisoService permisoService;
-	
+
 	@GetMapping("/periodo/new")
 	public ModelAndView formPeriodo() {
 		ModelAndView modelAndView = new ModelAndView("permiso/form-periodo");
 		modelAndView.addObject("permiso", new PermisoPeriodoModel());
-		
+
 		List<PersonaModel> personas = new ArrayList<PersonaModel>();
-		personas.add(new PersonaModel(1,3, "Pepe", "Jose"));
-		personas.add(new PersonaModel(2,4, "Roberto", "Jose"));
+		personas.add(new PersonaModel(1, 3, "Pepe", "Jose"));
+		personas.add(new PersonaModel(2, 4, "Roberto", "Jose"));
 		modelAndView.addObject("personas", personas);
-		
-		
+
 		List<RodadoModel> rodados = new ArrayList<RodadoModel>();
 		rodados.add(new RodadoModel(1, "AAA222", "Car-1"));
 		rodados.add(new RodadoModel(2, "AAA333", "Car-2"));
 		modelAndView.addObject("rodados", rodados);
-		
-		
+
 		return modelAndView;
 	}
-	
+
 	@PostMapping("/periodo/save")
-	public RedirectView save(
-			@Valid @ModelAttribute("permiso") PermisoPeriodoModel permisoModel, 
+	public RedirectView savePeriodo(@Valid @ModelAttribute("permiso") PermisoPeriodoModel permisoModel,
 			BindingResult bindingResult) {
 		RedirectView redirectView = new RedirectView("/");
-		
-		
+
 		permisoModel.setFecha(Date.valueOf(permisoModel.getFechaString()));
-		
-		//TODO: Traer a la persona correspondiente
-		//TODO: Traer al rodado correspondiente
-		//TODO: Fijarse que exista los lugares y en base a eso guardar o no
-		
+
+		// TODO: Traer a la persona correspondiente
+		// TODO: Traer al rodado correspondiente
+		// TODO: Fijarse que exista los lugares y en base a eso guardar o no
+
 		System.out.println(permisoModel);
-		
-		
-		
-		if(bindingResult.hasErrors())
+
+		if (bindingResult.hasErrors())
 			redirectView.setUrl("/permiso/periodo/new");
-		
-		
+
 		return redirectView;
 	}
+
+	@GetMapping("/dia/new")
+	public ModelAndView formDia() {
+		ModelAndView modelAndView = new ModelAndView("permiso/form-dia");
+		modelAndView.addObject("permiso", new PermisoDiarioModel());
+
+		List<PersonaModel> personas = new ArrayList<PersonaModel>();
+		personas.add(new PersonaModel(1, 3, "Pepe", "Jose"));
+		personas.add(new PersonaModel(2, 4, "Roberto", "Jose"));
+		modelAndView.addObject("personas", personas);
+
+		return modelAndView;
+	}
+
+	@PostMapping("/dia/save")
+	public RedirectView saveDia(@Valid @ModelAttribute("permiso") PermisoDiarioModel permisoModel,
+			BindingResult bindingResult) {
+		RedirectView redirectView = new RedirectView("/");
+
+		permisoModel.setFecha(Date.valueOf(permisoModel.getFechaString()));
+
+		// TODO: Traer a la persona correspondiente
+		// TODO: Fijarse que exista los lugares y en base a eso guardar o no
+
+		System.out.println(permisoModel);
+
+		if (bindingResult.hasErrors())
+			redirectView.setUrl("/permiso/periodo/new");
+
+		return redirectView;
+	}
+
 }
